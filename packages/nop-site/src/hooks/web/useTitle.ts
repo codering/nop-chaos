474c170ab6d051eaad637d5690b820b3ef98de42
codering <zcodeworld@gmail.com>
2024-06-28 16:22:27 +0800

    调整浏览器 title

--- packages/nop-site/src/hooks/web/useTitle.ts
+++ packages/nop-site/src/hooks/web/useTitle.ts
@@ -1,17 +1,17 @@
 import { watch, unref } from 'vue';
 import { useI18n } from '/@/hooks/web/useI18n';
 import { useTitle as usePageTitle } from '@vueuse/core';
-import { useGlobSetting } from '/@/hooks/setting';
 import { useRouter } from 'vue-router';
 import { useLocaleStore } from '/@/store/modules/locale';
 
 import { REDIRECT_NAME } from '/@/router/constant';
+import { useUserStore } from '/@/store/modules/user';
 
 /**
  * Listening to page changes and dynamically changing site titles
  */
 export function useTitle() {
-  const { title } = useGlobSetting();
+  const userStore = useUserStore();
   const { t } = useI18n();
   const { currentRoute } = useRouter();
   const localeStore = useLocaleStore();
@@ -26,9 +26,9 @@ export function useTitle() {
       if (route.name === REDIRECT_NAME) {
         return;
       }
-
+      const { appName } = userStore.getUserInfo;
       const tTitle = t(route?.meta?.title as string);
-      pageTitle.value = tTitle ? ` ${tTitle} - ${title} ` : `${title}`;
+      pageTitle.value = tTitle ? ` ${tTitle} - ${appName} ` : `${appName}`;
     },
     { immediate: true }
   );
